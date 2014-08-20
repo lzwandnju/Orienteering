202eab49aaa4e7cb474d6230e63a31202dc82ec2
ankittare <ankit.vader@gmail.com>
2014-08-20 12:17:32 +0530

    Main class and Position Class Created

---
+++ Orienteering/src/jp/co/worksap/global/Orienteering.java
@@ -0,0 +1,20 @@
+/**
+ * Main class for Orienteering application
+ */
+package jp.co.worksap.global;
+
+/**
+ * @author Dell
+ *
+ */
+public class Orienteering {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Orienteering/src/jp/co/worksap/global/Position.java
@@ -0,0 +1,25 @@
+package jp.co.worksap.global;
+
+public class Position {
+	
+	private int row;
+	private int col;
+	public Position(int row, int col) {
+		super();
+		this.row = row;
+		this.col = col;
+	}
+	public int getRow() {
+		return row;
+	}
+	public void setRow(int row) {
+		this.row = row;
+	}
+	public int getCol() {
+		return col;
+	}
+	public void setCol(int col) {
+		this.col = col;
+	}
+
+}
