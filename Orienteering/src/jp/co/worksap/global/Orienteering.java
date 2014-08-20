093b817b423cc115394b6ce3f53b8fd7979a22ed
ankittare <ankit.vader@gmail.com>
2014-08-20 17:03:49 +0530

    E

--- Orienteering/src/jp/co/worksap/global/Orienteering.java
+++ Orienteering/src/jp/co/worksap/global/Orienteering.java
@@ -3,6 +3,10 @@
  */
 package jp.co.worksap.global;
 
+import java.util.Hashtable;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * @author Dell
  *
@@ -15,8 +19,21 @@ public class Orienteering {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Map m=new Map(5,4);
-		m.createMap();
-		m.showMap();
+		//m.createMap();
+		//m.showMap();
+		Hashtable<Character,List<Position>> hash=new Hashtable<Character, List<Position>>();
+		List<Position> s=new LinkedList<Position>();
+		s.add( new Position(3,4));
+		s.add(new Position(5,6));
+		hash.put('@', s);
+		for(char ch:hash.keySet()){
+			for(Position p:hash.get(ch)){
+				System.out.println(p.getRow() + " "+ p.getCol());
+				
+			}
+		}
+		
+		
 	}
 
 }
